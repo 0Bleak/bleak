5d9e8a21b143cdc6e81ee244f25a9cd0b9de011e
0bleak <mr.tamimi.mohammed@gmail.com>
2025-03-02 00:30:32 +0100

    Fix Vite base path and HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; 
 import { ThemeProvider } from '@mui/material/styles';
 import RoutesComponent from './routing/RoutesComponent';
-import theme from './styles/theme';  
+import theme from './styles/theme';
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} >
       <Router>
         <RoutesComponent />
       </Router>
